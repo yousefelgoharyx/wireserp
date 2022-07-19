f31a7cdc6c19d6963a1a73eee2d8ed46c67c9e67
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2022-07-19 21:26:15 +0200

    fix date foramtting

--- src/pages/Store/Warehouses/Inventory.tsx
+++ src/pages/Store/Warehouses/Inventory.tsx
@@ -33,8 +33,8 @@ const Inventory = () => {
     async function handleSubmit(values: InventoryFormValues) {
         const newValues = {
             ...values,
-            from_date: dayjs(values.from_date).format('YYYY-MM-DD'),
-            to_date: dayjs(values.to_date).format('YYYY-MM-DD'),
+            from_date: values.from_date ? dayjs(values.from_date).format('YYYY-MM-DD') : null,
+            to_date: values.to_date ? dayjs(values.to_date).format('YYYY-MM-DD') : null,
         }
         try {
             await get(newValues);
