0c254efcef7b5d7ccd3408900a883fc9f5f50c01
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-07-26 21:32:49 +0200

    Sales bills

--- src/pages/Sales/Invoices/Invoices.tsx
+++ src/pages/Sales/Invoices/Invoices.tsx
@@ -1,13 +1,72 @@
+import useCreate from '../../../hooks/useCreate';
 import InvoicesForm from './InvoicesForm';
+import { showNotification } from '@mantine/notifications';
+import getApiError from './../../../utils/getApiError';
+import { useState } from 'react';
+import { useForm, yupResolver } from '@mantine/form';
+import { SaleBillForm } from './model/schema';
+import DataGrid from '../../../components/DataGrid';
+import { useProductsList } from './../../../api/useProducts';
+import find from '../../../utils/find';
+import { salesCols } from './model/columns';
+import { Stack } from '@mantine/core';
 
 const Invoices = () => {
-  function handleSubmit(values: SaleBillForm) {
-    console.log(values);
+  const form = useForm<SaleBillForm>({
+    validate: yupResolver(SaleBillForm),
+    initialValues: {
+      client_id: null,
+      date_time: new Date(),
+      warehouse_id: null,
+      value_added_tax: '1',
+      final_total: undefined,
+      product_id: null,
+      product_price: undefined,
+      quantity: undefined,
+      unit: 'unit',
+      quantity_price: undefined,
+    },
+  });
+  const [bill, setBill] = useState(null);
+  const [billItems, setBillItems] = useState<SaleBillForm[]>([]);
+  const { data: products } = useProductsList();
+  const { create, isCreating } = useCreate<SaleBillForm>(
+    ['sale-bills'],
+    '/sale-bills'
+  );
+  const salesData: SaleBillTable[] = billItems.map((bill) => ({
+    product_name: find(bill.product_id, products).product_name,
+    product_price: bill.product_price,
+    amount: `${bill.quantity} ${bill.unit}`,
+    total_price: bill.final_total,
+  }));
+
+  async function handleSubmit(values: SaleBillForm) {
+    try {
+      await create(values);
+      setBillItems([...billItems, values]);
+      showNotification({
+        message: 'Invoice created successfully',
+      });
+      form.reset();
+    } catch (error) {
+      showNotification({
+        message: getApiError(error.response.data),
+        color: 'red',
+      });
+    }
   }
   return (
-    <div>
-      <InvoicesForm onSubmit={handleSubmit} />
-    </div>
+    <Stack>
+      <InvoicesForm
+        form={form}
+        onSubmit={handleSubmit}
+        isLoading={isCreating}
+      />
+      {salesData.length > 0 && (
+        <DataGrid data={salesData} columns={salesCols} />
+      )}
+    </Stack>
   );
 };
 
